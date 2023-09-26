22601b665a63c9889dd47448036b0ae6bd586443
Marin <marin.muktic3@gmail.com>
2023-09-26 17:05:14 +0200

    app wrapped in theme switcher

--- src/Pages/Header/index.jsx
+++ src/Pages/Header/index.jsx
@@ -1,28 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../Header/header.scss";
 import kanbanLogoLight from "../../Icons/logo-light.svg";
 import boardImg from "../../Icons/icon-board.svg";
 import hideSidebar from "../../Icons/icon-hide-sidebar.svg";
+import { ThemeContext } from "../../Theme";
 
 const Header = () => {
   const boardIcon = <img src={boardImg} alt="board icon" />;
   const hideSidebarIcon = <img src={hideSidebar} alt="hide icon" />;
   const kanbanLogoLightIcon = <img src={kanbanLogoLight} alt="kanban logo" />;
 
-  
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
     <div className="headerContainer">
       <div className="headerContent">
         <h1>{kanbanLogoLightIcon}</h1>
         <p>ALL BOARDS</p>
-        <button>{boardIcon}Platform Launch</button>
-        <button>{boardIcon}Marketing Plan</button>
-        <button>{boardIcon}Roadmap</button>
+        <button className="board">{boardIcon}Platform Launch</button>
+        <button className="board">{boardIcon}Marketing Plan</button>
+        <button className="board">{boardIcon}Roadmap</button>
         <button>{boardIcon}+ Create New Board</button>
       </div>
       <div className="headerBottom">
-        <div>Here switch</div>
+        <div className="header-toggle-buttons">
+          <button onClick={() => toggleTheme()}>{theme}</button>
+        </div>
         <button>{hideSidebarIcon}Hide Sidebar</button>
       </div>
     </div>
