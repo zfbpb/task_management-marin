c5eeb4d72b20dc079bad430241a03577508fbd10
Marin <marin.muktic3@gmail.com>
2023-09-30 13:35:33 +0200

    header toggle theme

--- src/Pages/Header/index.jsx
+++ src/Pages/Header/index.jsx
@@ -1,22 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "../Header/header.scss";
+import "../../Assets/colors/colors.scss";
 
-import kanbanLogoLight from "../../Icons/logo-light.svg"
-import boardImg from "../../Icons/icon-board.svg"; 
+import kanbanLogoLight from "../../Icons/logo-light.svg";
+import kanbanLogoDark from "../../Icons/logo-dark.svg";
+import boardImg from "../../Icons/icon-board.svg";
 import hideSidebar from "../../Icons/icon-hide-sidebar.svg";
 import { ThemeContext } from "../../Theme";
 
 const Header = () => {
+  const [checkMode, setCheckMode] = useState(false);
+
   const boardIcon = <img src={boardImg} alt="board icon" />;
   const hideSidebarIcon = <img src={hideSidebar} alt="hide icon" />;
   const kanbanLogoLightIcon = <img src={kanbanLogoLight} alt="kanban logo" />;
-
+  const kanbanLogoDarkIcon = (
+    <img src={kanbanLogoDark} alt="kanban logo dark" />
+  );
   const { theme, toggleTheme } = useContext(ThemeContext);
 
+  const handleThemeToggle = () => {
+    toggleTheme();
+    setCheckMode((prev) => !prev); 
+  };
   return (
-    <header className="header">
+    <header className={`header ${theme}`}>
       <div className="header__content">
-        <h1>{kanbanLogoLightIcon}</h1>
+        {checkMode ? (
+          <h1>{kanbanLogoDarkIcon}</h1>
+        ) : (
+          <h1>{kanbanLogoLightIcon}</h1>
+        )}
+
         <p>ALL BOARDS</p>
         <button className="board">{boardIcon}Platform Launch</button>
         <button className="board">{boardIcon}Marketing Plan</button>
@@ -25,7 +40,7 @@ const Header = () => {
       </div>
       <div className="header__bottom">
         <div className="header-toggle-buttons">
-          <button onClick={() => toggleTheme()}>{theme}</button>
+          <button onClick={() => handleThemeToggle()}>{theme}</button>
         </div>
         <button>{hideSidebarIcon}Hide Sidebar</button>
       </div>
