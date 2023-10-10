84d7686f8aaf30ad35aae81abce806f64c7f70c5
Marin <marin.muktic3@gmail.com>
2023-10-10 12:30:35 +0200

    close btn function

--- src/Components/CreateBoard/index.jsx
+++ src/Components/CreateBoard/index.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import './board.scss'
-const CreateBoard = () => {
-  return (
-    <div className='create-boardContainer'>
-      <p>Create Board</p>
-    </div>
-  )
+import React from "react";
+import "./board.scss";
+const CreateBoard = ({setCreateBoard}) => {
+  
+const handleClose = () =>{
+  setCreateBoard(false)
 }
+  return (
+    <div className="create-boardContainer">     
+        <p className="board-title">Create Board</p>
+        <button className="close-btn" onClick={handleClose}>X</button>   
+    </div>
+  );
+};
 
-export default CreateBoard
+export default CreateBoard;
