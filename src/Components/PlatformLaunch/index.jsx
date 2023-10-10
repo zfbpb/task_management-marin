a29f40f3b22e3b8b15ab3316abd892770e8a847c
Marin <marin.muktic3@gmail.com>
2023-10-10 15:08:33 +0200

    task name fetch

--- src/Components/PlatformLaunch/index.jsx
+++ src/Components/PlatformLaunch/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./platform.scss"
+import "./platform.scss";
 import initialData from "../../Assets/data/data.json";
 
 const PlatformLaunch = () => {
@@ -26,17 +26,30 @@ const PlatformLaunch = () => {
   };
  */
   //console.log("some", initialData);
-  //console.log("boards", data.boards[0].name)
+  //console.log("boards", data.boards[0])
+
+  /* 
+  const { theme } = useContext(ThemeContext);
+  <div className={`platform-container ${theme}`}></div> 
+  */
   return (
     <div className="platform-container">
-      {data.boards &&
-      <h4>{data.boards[0].name}</h4>
-      }
-      {data.boards && data.boards.map((board) => (
-        <div key={board.id} className="board">{board[0]}</div>
+      {data.boards && <h4>{data.boards[0].name}</h4>}
+
+      {/* <p>{data.boards?.[0].columns?.[0].tasks?.[0].title}</p>
+      <p>{data.boards?.[0].columns?.[0].tasks?.[1].title}</p>
+      <p>{data.boards?.[0].columns?.[0].tasks?.[2].title}</p>
+      <p>{data.boards?.[0].columns?.[0].tasks?.[3].title}</p> */}
+
+      {data.boards?.[0]?.columns?.[0]?.tasks?.map((task) => (
+        <p key={task.id}>{task.title}</p>
       ))}
+
+      {/* {data.boards && data.boards.map((board) => (
+        <div key={board.id} className="board">{board[0]}</div>
+      ))} */}
     </div>
   );
-}
+};
 
 export default PlatformLaunch;
