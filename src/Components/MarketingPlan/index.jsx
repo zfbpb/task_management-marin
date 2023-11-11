b00976298d11c109e732b8886c12dc9538d75fd4
Marin <marin.muktic3@gmail.com>
2023-11-11 20:06:08 +0100

    intial fetch

--- src/Components/MarketingPlan/index.jsx
+++ src/Components/MarketingPlan/index.jsx
@@ -1,14 +1,66 @@
 import React from "react";
 import initialData from "../../Assets/data/data.json";
 import "./marketing.scss"
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+import Card from "../../Assets/drag-drop/Card";
 const MarketingPlan = () => {
   const data = initialData;
+
+/*   const columnColors = {
+    Todo: "blue-ball",
+    Doing: "purple-ball",
+  }; */
+const onDragEnd = () =>{
+  return;
+}
   return (
     <div className="marketing-container">
       <div className="title">
         <h1>{data.boards?.[1].name}</h1>
       </div>
-      <div className="empty"></div>
+      <div className="platform-wrapper-vertical">
+        <div className="platform-wrapper-horizontal">
+          <DragDropContext onDragEnd={onDragEnd}>
+            {data.boards?.[0].columns.map((column, index) => {
+
+              const columnNum = column.tasks?.length;
+              //const columnColorClass = columnColors[column.name] || "ball";
+
+              
+              return (
+                <div className="column-wrapper" key={column.id}>
+                  <p>
+                    <span className={`ball `}></span>{/* ${columnColorClass} */}
+                    {column.name}({columnNum})
+                  </p>
+
+                  <Droppable droppableId={column.id.toString()}>
+                    {(provided) => (
+                      <div ref={provided.innerRef} {...provided.droppableProps}>
+                        {data.boards?.[1]?.columns?.[index]?.tasks?.map(
+                          (task, taskIndex) => (
+                            <Card
+                              key={task.id}
+                              id={task.id}
+                              text={task.title}
+                              index={taskIndex}
+                            />
+                          )
+                        )}
+                        {provided.placeholder}
+                        {/* {isEmptyColumn[column.id] && ( */}
+                          <div className="empty">Empty</div>
+                       {/*  )} */}
+                      </div>
+                    )}
+                  </Droppable>
+                </div>
+              );
+            })}
+          </DragDropContext>
+          <div className="new-column">New Column +</div>
+        </div>
+      </div>
     </div>
   );
 };
